0bd24ccaac4e1828918d6439d97960187429f543
Ben Hartling <w0400132@nscc.ca>
2019-04-17 15:39:19 -0300

    Showing list of 2 dummy posts, not specifics yet

--- SharpJokes/SharpJokes/Models/PostModel.cs
+++ SharpJokes/SharpJokes/Models/PostModel.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace SharpJokes.Models
 {
-    class PostModel
+    public class PostModel
     {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Link { get; set; }
+        public string UserName { get; set; }
+        public int PostId { get; set; }
+
+        public PostModel(string title, string body, string link, string username, int postID)
+        {
+            Title = title;
+            Body = body;
+            Link = link;
+            UserName = username;
+            PostId = postID;
+        }
     }
+
 }
