6bdbf4cbd7244ae9eb3142976ba4d877d51133cf
Michael Gosling <contact@michaelgosling.me>
2019-04-17 10:55:03 -0300

    Add refresh token, app secret constants to RedditAccess, add static redditapi reference.

--- SharpJokes/SharpJokes/RedditAccess.cs
+++ SharpJokes/SharpJokes/RedditAccess.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Reddit;
 
 namespace SharpJokes
 {
-    class RedditAccess
+    public class RedditAccess
     {
-
+        private const string REFRESH_TOKEN = "";
+        private const string APP_SECRET = "";
+        private static RedditAPI api = new RedditAPI(appId: "SharpJokes", appSecret: APP_SECRET, refreshToken: REFRESH_TOKEN);
     }
 }
