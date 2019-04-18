38538bc5e2d7b509ed2682a21ad52dd0fa2d0976
Ben Hartling <w0400132@nscc.ca>
2019-04-18 14:57:40 -0300

    Working on displaying specific

--- SharpJokes/SharpJokes/ViewModels/PostViewModel.cs
+++ SharpJokes/SharpJokes/ViewModels/PostViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace SharpJokes.ViewModels
 {
     public class PostViewModel
     {
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        private PostModel _selectedPost;
         public ObservableCollection<PostModel> Posts { get; set; }
         public PostViewModel()
         {
@@ -17,6 +21,28 @@ namespace SharpJokes.ViewModels
             Posts.Add(testPost1);
             PostModel testPost2 = new PostModel("Title 2", "Body 2", null, "UserName2", 2);
             Posts.Add(testPost2);
+        }
+        public PostModel SelectedPost
+        {
+            get { return _selectedPost; }
+            set
+            {
+                _selectedPost = value;
+                if (value == null)
+                {
+                    
+                }
+                else
+                {
+                    string title = value.Title;
+                    
+                }
+                
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Posts"));
+                //Event to call the save functionality
+                //AcceptCommand.FireCanExecuteChanged();
+            }
+
         }
     }
 }
